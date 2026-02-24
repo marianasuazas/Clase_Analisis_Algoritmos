7cbc2170181be1079f9bddf7a5c11c0c81773c61
marianasuazas <125661779+marianasuazas@users.noreply.github.com>
2026-02-23 21:00:26 -0500

    create fuerza bruta.py

---
+++ Parcial1/Ejercicio 4/fuerza bruta.py
@@ -0,0 +1,28 @@
+"Complejidad: O(2^n · n) porque se generan 2^n combinaciones y para cada una se recorren hasta n elementos; mejor y peor caso O(2^n · n); espacio O(n)."
+def fuerza_bruta(n, s):
+    min_eliminadas = n
+ 
+    for mask in range(1 << n):
+        nueva = []
+ 
+        for i in range(n):
+            if mask & (1 << i):
+                nueva.append(s[i])
+ 
+        valido = True
+        for i in range(1, len(nueva)):
+            if nueva[i] == nueva[i - 1]:
+                valido = False
+                break
+ 
+        if valido:
+            eliminadas = n - len(nueva)
+            min_eliminadas = min(min_eliminadas, eliminadas)
+ 
+    return min_eliminadas
+ 
+ 
+n = int(input().strip())
+s = input().strip()
+ 
+print(fuerza_bruta(n, s))
