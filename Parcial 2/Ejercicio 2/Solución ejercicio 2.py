a86e9da68496b35787b7b008e687ca605943d2f4
Manu0412 <163077392+Manu0412@users.noreply.github.com>
2026-03-11 22:06:51 -0500

    Update Solución ejercicio 2.py
    Cambio en el comentario del código

--- Parcial 2/Ejercicio 2/Solución ejercicio 2.py
+++ Parcial 2/Ejercicio 2/Solución ejercicio 2.py
@@ -4,7 +4,7 @@ class Solution:
         """
         Complejidad Temporal: O(n log n)
         Complejidad Espacial: O(1)
-        Se ordenan los intervalos por su final y se recorren comparando con el último intervalo válido; si se solapan, se cuenta como eliminado. 
+        Se ordenan los intervalos por su final y se recorren comparando con el último intervalo válido; si se solapan, se cuenta como eliminado, lo que minimiza los solapamientos y deja más espacios para los siguientes. 
         """
 
         intervals.sort(key=lambda x: x[1])
