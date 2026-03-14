63a5257bb52e4a52a1fe5ea96b0fbb524b11596e
Sofía Ortiz Serna <sofiaortizserna684@gmail.com>
2026-03-14 11:32:32 -0500

    Se agrega explicaación de código
    Se agrega explicaación de código

--- Parcial 2/Ejercicio1/Ejercicio_1.py
+++ Parcial 2/Ejercicio1/Ejercicio_1.py
@@ -1,4 +1,11 @@
 def findContentChildren(g, s):
+
+    #EXPLICACIÓN.
+    #Empezamos ordenando las listas de niños y galletas.
+    #Luego creamos las variables de i y j para recorrer las listas.
+    #Se compara la galleta con el hambre del niño
+    #Si la galleta es suficiente Se aumenta el contador y se avanza al siguiente niño, si la galleta no es suficiente continuamos con una mas grande para el mismo niño.
+    #
     
     #O(n log n + m log m)temporal por los 2 sort para las listas.
     #O(1) espacial, usamos i, j, contentos.
